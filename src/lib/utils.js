44323db630bdbb293378db5579f2dfdf873f8cb5
iVishav48 <vishav4848@gmail.com>
2025-10-25 19:24:43 +0530

    fixed uploading a image

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,5 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
-    console.error("Error processing image:", error)
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
